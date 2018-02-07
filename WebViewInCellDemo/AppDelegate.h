1c7de0781fae7923e1a62c09b2785c47b0bd5713
caihongguang <15052422@qq.com>
2018-02-07 08:49:20 +0800

    Initial Commit

---
+++ WebViewInCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewInCellDemo
+//
+//  Created by caihongguang on 2018/2/7.
+//  Copyright © 2018年 caihongguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewInCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewInCellDemo
+//
+//  Created by caihongguang on 2018/2/7.
+//  Copyright © 2018年 caihongguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
